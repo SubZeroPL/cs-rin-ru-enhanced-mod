1b21f8b67b7a5c0e6f8a372057bc17867c4ccda6
Matthew Lim <matthewlim230505@gmail.com>
2023-06-11 22:06:46 +0800

    Improved SteamDB links and user tags.
    Renamed `steamDBLinkSpoiler() and `steamDBLink()`. Fixed `steamDBLink()` to work with links in the same line and in spoilers. Moved `addUsersTag()` before `steamDBLink()` so it makes the logic a bit simpler and doesn't cause errors on non-topic pages.

--- cs-rin-ru-enhanced-mod.user.js
+++ cs-rin-ru-enhanced-mod.user.js
@@ -5,7 +5,7 @@
 // @name:fr         CS.RIN.RU Amélioré
 // @name:pt         CS.RIN.RU Melhorado
 // @namespace       Royalgamer06
-// @version         0.7.3
+// @version         0.7.4
 // @description     Enhance your experience at CS.RIN.RU - Steam Underground Community.
 // @description:fr  Améliorez votre expérience sur CS.RIN.RU - Steam Underground Community.
 // @description:pt  Melhorar a sua experiência no CS.RIN.RU - Steam Underground Community.
@@ -282,7 +282,7 @@ function functionsCalledByInfiniteScrolls(data) {
     hideScs();
     setupTopicPreview();
     addLink();
-    steamdbLink();
+    steamDBLink();
     goToUnreadPosts();
 }
 
@@ -526,24 +526,65 @@ function setupTopicPreview() {
 setupTopicPreview();
 
 /*
-Made by Altansar
+Made by Redpoint
+And adapted for cs.rin.ru enhanced by Altansar
 */
-function steamdbLinkSpoiler() { //Calls the function every time a spoiler is unfolded
-    if (options.steam_db_link) {
-        const elements = document.querySelectorAll('button[type="button"],input[type="submit"], input[type="button"], input[value="show"]');
-        for (let elementNumber = 0; elementNumber < elements.length; ++elementNumber) {
-            elements[elementNumber].addEventListener("click", steamdbLink);
+function addUsersTag() {
+    if (options.add_users_tag) {
+        const steamLink = $('a[href^="https://store.steampowered.com/app/"], a[href^="http://store.steampowered.com/app/"]').first()[0];
+        if (steamLink != null) {
+            let textContent = $(steamLink).nextAll('br').first().next().text();
+            if (textContent === "Genre(s):") { //if we are on the game presentation page
+                // Get the link to the Steam game page
+                const link = steamLink.href;
+                // Send a request to the Steam game page and bypass CSP
+                GM_xmlhttpRequest({
+                    method: "GET", url: link, onload: function (response) {
+                        // Parse the response as HTML
+                        const parser = new DOMParser();
+                        const doc = parser.parseFromString(response.responseText, "text/html");
+                        // Get the genre tags from the response
+                        const tags = doc.querySelectorAll("#glanceCtnResponsiveRight > div.glance_tags_ctn.popular_tags_ctn > div.glance_tags.popular_tags > a.app_tag");
+                        // Extract the text content of each tag and join them with a comma and a space
+                        const genres = Array.from(tags).map(tag => tag.textContent.trim()).join(", ");
+                        // Modify the original page by adding a new line with the genres
+                        // const br = document.querySelector("#pagecontent > table:nth-child(" + child + ") > tbody > tr:nth-child(3) > td:nth-child(2) > table > tbody > tr > td > div > br:nth-child(16)");
+                        const br = $('span[style="font-weight: bold"]:contains("Genre(s):")').next()[0];
+                        const span = document.createElement("span");
+                        span.style.fontWeight = "bold";
+                        span.textContent = "User-defined Tag(s): ";
+                        const text = document.createTextNode(genres);
+                        br.parentNode.insertBefore(document.createElement("br"), br);
+                        br.parentNode.insertBefore(span, br);
+                        br.parentNode.insertBefore(text, br);
+                    }
+                });
+            }
         }
     }
 }
 
-steamdbLinkSpoiler();
+addUsersTag()
+
+/*
+Made by Altansar
+*/
+function steamDBLinkSpoiler() { //Calls the function every time a spoiler is unfolded
+    if (options.steam_db_link) {
+        const elements = document.querySelectorAll('button[type="button"],input[type="submit"], input[type="button"], input[value="show"]');
+        for (let elementNumber = 0; elementNumber < elements.length; ++elementNumber) {
+            elements[elementNumber].addEventListener("click", steamDBLink);
+        }
+    }
+}
+
+steamDBLinkSpoiler();
 
 /*
 Made by Altansar
 Remade by Redpoint
 */
-function steamdbLink() {
+function steamDBLink() {
     if (!options.steam_db_link || this.value === "Show") {
         return;
     }
@@ -557,25 +598,30 @@ function steamdbLink() {
         if (steamLink.match("://store.steampowered.com/app")) {
             let slash = steamLink.endsWith('/');
             steamLink = slash ? steamLink.slice(0, -1) : steamLink;
+
             let splits = steamLink.split("/");
             if (splits[splits.length - 1].match(/^[0-9]+$/)) {
                 steamLink = splits[splits.length - 1];
             } else if (splits[splits.length - 2].match(/^[0-9]+$/)) {
                 steamLink = splits[splits.length - 2];
             }
+
             let DBlink = `https://steamdb.info/app/${steamLink}${slash ? '/' : ''}`;
-            while ((i + 1 < postlinks.length) && (postlinks[i].getBoundingClientRect().y === postlinks[i + 1].getBoundingClientRect().y)) {
-                i++;
+            let j = i;
+            console.log($(postlinks[j]).next().prop("tagName") !== "BR");
+            while ((j + 1 < postlinks.length) && (postlinks[j].getBoundingClientRect().y === postlinks[j + 1].getBoundingClientRect().y) && postlinks[j].nextSibling !== null) {
+                j++;
             }
-            if ((i + 1 >= postlinks.length) || !postlinks[i + 1].text.match(DBlink)) {
-                document.getElementsByClassName("postlink")[i].insertAdjacentHTML("afterend", "<a href=" + DBlink + " class=\"postlink\" rel=\"nofollow\">" + DBlink + "</a>"); //Write the link (right part)
-                document.getElementsByClassName("postlink")[i].insertAdjacentHTML("afterend", "<br><span style=\"font-weight: bold\">  <svg version=\"1.1\" width=\"1.3em\" height=\"1.3em\" viewBox=\"0 0 128 128\" fill=#bbbbbb class=\"octicon octicon-steamdb\" aria-hidden=\"true\"><path fill-rule=\"evenodd\" d=\"M63.9 0C30.5 0 3.1 11.9.1 27.1l35.6 6.7c2.9-.9 6.2-1.3 9.6-1.3l16.7-10c-.2-2.5 1.3-5.1 4.7-7.2 4.8-3.1 12.3-4.8 19.9-4.8 5.2-.1 10.5.7 15 2.2 11.2 3.8 13.7 11.1 5.7 16.3-5.1 3.3-13.3 5-21.4 4.8l-22 7.9c-.2 1.6-1.3 3.1-3.4 4.5-5.9 3.8-17.4 4.7-25.6 1.9-3.6-1.2-6-3-7-4.8L2.5 38.4c2.3 3.6 6 6.9 10.8 9.8C5 53 0 59 0 65.5c0 6.4 4.8 12.3 12.9 17.1C4.8 87.3 0 93.2 0 99.6 0 115.3 28.6 128 64 128c35.3 0 64-12.7 64-28.4 0-6.4-4.8-12.3-12.9-17 8.1-4.8 12.9-10.7 12.9-17.1 0-6.5-5-12.6-13.4-17.4 8.3-5.1 13.3-11.4 13.3-18.2 0-16.5-28.7-29.9-64-29.9zm22.8 14.2c-5.2.1-10.2 1.2-13.4 3.3-5.5 3.6-3.8 8.5 3.8 11.1 7.6 2.6 18.1 1.8 23.6-1.8s3.8-8.5-3.8-11c-3.1-1-6.7-1.5-10.2-1.5zm.3 1.7c7.4 0 13.3 2.8 13.3 6.2 0 3.4-5.9 6.2-13.3 6.2s-13.3-2.8-13.3-6.2c0-3.4 5.9-6.2 13.3-6.2zM45.3 34.4c-1.6.1-3.1.2-4.6.4l9.1 1.7a10.8 5 0 1 1-8.1 9.3l-8.9-1.7c1 .9 2.4 1.7 4.3 2.4 6.4 2.2 15.4 1.5 20-1.5s3.2-7.2-3.2-9.3c-2.6-.9-5.7-1.3-8.6-1.3zM109 51v9.3c0 11-20.2 19.9-45 19.9-24.9 0-45-8.9-45-19.9v-9.2c11.5 5.3 27.4 8.6 44.9 8.6 17.6 0 33.6-3.3 45.2-8.7zm0 34.6v8.8c0 11-20.2 19.9-45 19.9-24.9 0-45-8.9-45-19.9v-8.8c11.6 5.1 27.4 8.2 45 8.2s33.5-3.1 45-8.2z\"></path></svg> SteamDB:</span> "); //write left part
+            console.log("i: " + i + " j: " + j);
+            if ((j + 1 >= postlinks.length) || !postlinks[j + 1].text.match(DBlink)) {
+                postlinks[j].insertAdjacentHTML("afterend", "<a href=" + DBlink + " class=\"postlink\" rel=\"nofollow\">" + DBlink + "</a>"); // Write the link (right part)
+                postlinks[j].insertAdjacentHTML("afterend", "<br><span style=\"font-weight: bold\">  <svg version=\"1.1\" width=\"1.3em\" height=\"1.3em\" viewBox=\"0 0 128 128\" fill=#bbbbbb class=\"octicon octicon-steamdb\" aria-hidden=\"true\"><path fill-rule=\"evenodd\" d=\"M63.9 0C30.5 0 3.1 11.9.1 27.1l35.6 6.7c2.9-.9 6.2-1.3 9.6-1.3l16.7-10c-.2-2.5 1.3-5.1 4.7-7.2 4.8-3.1 12.3-4.8 19.9-4.8 5.2-.1 10.5.7 15 2.2 11.2 3.8 13.7 11.1 5.7 16.3-5.1 3.3-13.3 5-21.4 4.8l-22 7.9c-.2 1.6-1.3 3.1-3.4 4.5-5.9 3.8-17.4 4.7-25.6 1.9-3.6-1.2-6-3-7-4.8L2.5 38.4c2.3 3.6 6 6.9 10.8 9.8C5 53 0 59 0 65.5c0 6.4 4.8 12.3 12.9 17.1C4.8 87.3 0 93.2 0 99.6 0 115.3 28.6 128 64 128c35.3 0 64-12.7 64-28.4 0-6.4-4.8-12.3-12.9-17 8.1-4.8 12.9-10.7 12.9-17.1 0-6.5-5-12.6-13.4-17.4 8.3-5.1 13.3-11.4 13.3-18.2 0-16.5-28.7-29.9-64-29.9zm22.8 14.2c-5.2.1-10.2 1.2-13.4 3.3-5.5 3.6-3.8 8.5 3.8 11.1 7.6 2.6 18.1 1.8 23.6-1.8s3.8-8.5-3.8-11c-3.1-1-6.7-1.5-10.2-1.5zm.3 1.7c7.4 0 13.3 2.8 13.3 6.2 0 3.4-5.9 6.2-13.3 6.2s-13.3-2.8-13.3-6.2c0-3.4 5.9-6.2 13.3-6.2zM45.3 34.4c-1.6.1-3.1.2-4.6.4l9.1 1.7a10.8 5 0 1 1-8.1 9.3l-8.9-1.7c1 .9 2.4 1.7 4.3 2.4 6.4 2.2 15.4 1.5 20-1.5s3.2-7.2-3.2-9.3c-2.6-.9-5.7-1.3-8.6-1.3zM109 51v9.3c0 11-20.2 19.9-45 19.9-24.9 0-45-8.9-45-19.9v-9.2c11.5 5.3 27.4 8.6 44.9 8.6 17.6 0 33.6-3.3 45.2-8.7zm0 34.6v8.8c0 11-20.2 19.9-45 19.9-24.9 0-45-8.9-45-19.9v-8.8c11.6 5.1 27.4 8.2 45 8.2s33.5-3.1 45-8.2z\"></path></svg> SteamDB:</span> "); // Write left part
             }
         }
     }
 }
 
-steamdbLink();
+steamDBLink();
 
 /*
 Originally made by mandus
@@ -696,47 +742,6 @@ function fetchChat() {
             chatContainer.appendChild(script);
         });
 }
-
-/*
-Made by Redpoint
-And adapted for cs.rin.ru enhanced by Altansar
-*/
-function addUsersTag() {
-    if (options.add_users_tag) {
-        const steamLink = $('a[href^="https://store.steampowered.com/app/"], a[href^="http://store.steampowered.com/app/"]').first()[0];
-        if (steamLink != null) {
-            let textContent = $(steamLink).nextAll('br').first().next().text();
-            if (textContent === "Genre(s):" || textContent === "   SteamDB:") { //if we are on the game presentation page
-                // Get the link to the Steam game page
-                const link = steamLink.href;
-                // Send a request to the Steam game page and bypass CSP
-                GM_xmlhttpRequest({
-                    method: "GET", url: link, onload: function (response) {
-                        // Parse the response as HTML
-                        const parser = new DOMParser();
-                        const doc = parser.parseFromString(response.responseText, "text/html");
-                        // Get the genre tags from the response
-                        const tags = doc.querySelectorAll("#glanceCtnResponsiveRight > div.glance_tags_ctn.popular_tags_ctn > div.glance_tags.popular_tags > a.app_tag");
-                        // Extract the text content of each tag and join them with a comma and a space
-                        const genres = Array.from(tags).map(tag => tag.textContent.trim()).join(", ");
-                        // Modify the original page by adding a new line with the genres
-                        // const br = document.querySelector("#pagecontent > table:nth-child(" + child + ") > tbody > tr:nth-child(3) > td:nth-child(2) > table > tbody > tr > td > div > br:nth-child(16)");
-                        const br = $('span[style="font-weight: bold"]:contains("Genre(s):")').next()[0];
-                        const span = document.createElement("span");
-                        span.style.fontWeight = "bold";
-                        span.textContent = "User-defined Tag(s): ";
-                        const text = document.createTextNode(genres);
-                        br.parentNode.insertBefore(document.createElement("br"), br);
-                        br.parentNode.insertBefore(span, br);
-                        br.parentNode.insertBefore(text, br);
-                    }
-                });
-            }
-        }
-    }
-}
-
-addUsersTag()
 
 /*
 Made by Altansar
