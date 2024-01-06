0139566526b51cd66e9d7aa39a23d2e9329f0635
Matthew Lim <matthewlim230505@gmail.com>
2024-01-06 10:35:22 +0800

    Code cleanup
    Fixed spacing, change var to let, and add "Re: " text before post link

--- cs-rin-ru-enhanced-mod.user.js
+++ cs-rin-ru-enhanced-mod.user.js
@@ -37,7 +37,7 @@ Contributor: odusi (https://cs.rin.ru/forum/memberlist.php?mode=viewprofile&u=58
 Contributor: Mandus (https://cs.rin.ru/forum/memberlist.php?mode=viewprofile&u=1487447) has created the original function to copy the link from a message
 */
 
-const CONFIG_PAGE = "https://raw.githubusercontent.com/SubZeroPL/cs-rin-ru-enhanced-mod/master/config.html"
+const CONFIG_PAGE = "https://raw.githubusercontent.com/SubZeroPL/cs-rin-ru-enhanced-mod/beta/config.html"
 
 const AJAX_LOADER = `
 <div style="margin-left: 50%;">
@@ -408,9 +408,9 @@ if (URLContains("posting.php" && "do=mention") && options.mentioning) {
     const p = URLParam("p");
     const u = URLParam("u");
     const a = URLParam("a");
-    var postBody = `@[url=${FORUM_BASE_URL}memberlist.php?mode=viewprofile&u=${u}]${decodeURI(a)}[/url], `;
-    if(options.mentioning==2) { //Author and post
-        postBody += `[url=http://cs.rin.ru/forum/viewtopic.php?p=${p}#p${p}]Post[/url]. `;
+    let postBody = `@[url=${FORUM_BASE_URL}memberlist.php?mode=viewprofile&u=${u}]${decodeURI(a)}[/url], `;
+    if (options.mentioning === 2) { //Author and post
+        postBody += `Re: [url=http://cs.rin.ru/forum/viewtopic.php?p=${p}#p${p}]Post[/url]. `;
     }
     $("[name=message]").val(postBody);
 }
