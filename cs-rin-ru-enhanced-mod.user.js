7e2f877d3d92ef2217de72c578159c28c1eb6cf1
Reddiepoint <31847741+Reddiepoint@users.noreply.github.com>
2024-10-26 22:30:07 +0800

    Bump version

--- cs-rin-ru-enhanced-mod.user.js
+++ cs-rin-ru-enhanced-mod.user.js
@@ -5,7 +5,7 @@
 // @name:fr         CS.RIN.RU Amélioré
 // @name:pt         CS.RIN.RU Melhorado
 // @namespace       Royalgamer06
-// @version         1.2.0
+// @version         1.2.1
 // @description     Enhance your experience at CS.RIN.RU - Steam Underground Community.
 // @description:fr  Améliorez votre expérience sur CS.RIN.RU - Steam Underground Community.
 // @description:pt  Melhorar a sua experiência no CS.RIN.RU - Steam Underground Community.
