b19e61bbe65ec2cc9e5508fe64253bf585387662
SubZeroPL <subzeropl@gmail.com>
2026-01-22 11:18:15 +0100

    Bump version to 1.2.6

--- cs-rin-ru-enhanced-mod.user.js
+++ cs-rin-ru-enhanced-mod.user.js
@@ -5,7 +5,7 @@
 // @name:fr         CS.RIN.RU Amélioré
 // @name:pt         CS.RIN.RU Melhorado
 // @namespace       Royalgamer06
-// @version         1.2.5
+// @version         1.2.6
 // @description     Enhance your experience at CS.RIN.RU - Steam Underground Community.
 // @description:fr  Améliorez votre expérience sur CS.RIN.RU - Steam Underground Community.
 // @description:pt  Melhorar a sua experiência no CS.RIN.RU - Steam Underground Community.
